9979217c358d2d2d12a8ca386c93f99949455452
Your Name <you@example.com>
2017-05-03 00:03:09 +0900

    first

---
+++ nn.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      U16
+#
+# Created:     02/05/2017
+# Copyright:   (c) U16 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def odd_or_even():
+    for i in range(1,6):
+        if i % 2 != 0:
+            print("{0} is odd".format(i))
+        else:
+            print("{0} is even".format(i))
+
+
+if __name__ == "__main__":
+    odd_or_even()
